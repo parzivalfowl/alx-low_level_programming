14b6cb91bc882f34e4eae41e17f89ce5785faa11
parzivalfowl@gmail.com <parzivalfowl@gmaiml.com>
2021-09-30 18:53:32 +0300

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-prints a string
+ *
+ * Return: returns a value of 0 if successful
+ */
+int main(void)
+{
+printf( "\"Programming is like building a multilingual puzzle";)
+return (0);
+}
